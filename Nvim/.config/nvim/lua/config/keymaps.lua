dfc87a0415ce71b50a3cf8f6e478f53ef70f35f0
JakobEdvardsson <jakob.edvardsson@outlook.com>
2024-02-04 21:36:03 +0100

    Added Nvim and fixed paths

---
+++ Nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,32 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- buffers
+vim.keymap.set("n", "öö", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "ää", "<cmd>bnext<cr>", { desc = "Next buffer" })
+
+vim.keymap.set("n", "öq", vim.cmd.cprev, { desc = "Previous quickfix" })
+vim.keymap.set("n", "äq", vim.cmd.cnext, { desc = "Next quickfix" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+
+vim.keymap.set("n", "äd", diagnostic_goto(true), { desc = "Next Diagnostic" })
+vim.keymap.set("n", "öd", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+vim.keymap.set("n", "äe", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+vim.keymap.set("n", "öe", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+vim.keymap.set("n", "äw", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+vim.keymap.set("n", "öw", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- tabs
+
+vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
